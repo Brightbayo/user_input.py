548985a7e40f6b298afa9936b8097c4cb41c64d4
Adebayoabdurrahmanadewale <adebayowale003@gmail.com>
2024-08-26 15:20:47 +0100

    Initial commit

---
+++ py/my_list.py
@@ -0,0 +1,56 @@
+# # Initialize an empty list
+# my_list = []
+
+# # Append elements to the list
+# my_list.append(10)
+# my_list.append(20)
+# my_list.append(30)
+# my_list.append(40)
+
+# # Print the updated list
+# print(my_list)
+
+# # Initialize the list
+# my_list = [10, 20, 30, 40]
+
+# # Insert 15 at the second position (index 1)
+# my_list.insert(1, 15)
+
+# # Print the updated list
+# print(my_list)
+
+# # Initialize the list
+# my_list = [10, 15, 20, 30, 40]
+
+# # Extend my_list with another list
+# my_list.extend([50, 60, 70])
+
+# # Print the updated list
+# print(my_list)
+
+# # Initialize the list
+# my_list = [10, 15, 20, 30, 40, 50, 60, 70]
+
+# # Remove the last element from the list
+# my_list.pop()
+
+# # Print the updated list
+# print(my_list)
+
+# # Initialize the list
+# my_list = [10, 15, 20, 30, 40, 50, 60]
+
+# # Sort the list in ascending order
+# my_list.sort()
+
+# # Print the sorted list
+# print(my_list)
+
+# # Initialize the list
+# my_list = [10, 15, 20, 30, 40, 50, 60]
+
+# # Find the index of the value 30
+# index = my_list.index(30)
+
+# # Print the index
+# print("The index of 30 is:", index)
